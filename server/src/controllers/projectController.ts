41ae0dfe17889f41b43711e150713d04b0cc10a5
massmr <marcellin.massimo@gmail.com>
2025-05-17 23:56:46 +0200

    feat: add get getAll delete for projects

--- server/src/controllers/projectController.ts
+++ server/src/controllers/projectController.ts
@@ -1,16 +1,42 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import { AuthenticatedUser, AuthRequest } from "../types/Auth";
-import { handleProjectCreation } from "../services/projectService";
+import { handleProjectCreation, handleProjectDeletion, handleAllProjectsFetch, handleProjectByIdFetch } from "../services/projectService";
 
 export const createProject = async(req: AuthRequest, res: Response) => {
     try {
-
         const user = req.user as AuthenticatedUser;
-        
         const project = await handleProjectCreation(user, req.body)
-
         res.status(201).json({ project });
     } catch (error) {
         res.status(500).json({ message: "Erreur serveur lors de la création du projet.", error });
     }
 }
+
+export const deleteProject = async(req: AuthRequest, res: Response) => {
+    try {
+        const user = req.user as AuthenticatedUser;
+        const projectId = req.params.id;
+        const result = await handleProjectDeletion(user, projectId);
+        res.status(200).json({ message: "Le projet a été supprimé", result});
+    } catch (error) {
+        res.status(500).json({ message: "Erreur serveur lors de la suppression du projet.", error });
+    }
+}
+
+export const getAllProjects = async (_req: Request, res: Response) => {
+  try {
+    const projects = await handleAllProjectsFetch();
+    res.status(200).json({ projects });
+  } catch (error: any) {
+    res.status(500).json({ message: "Erreur serveur lors de la récupération des projets.", error });
+  }
+};
+
+export const getProjectById = async (req: AuthRequest, res: Response) => {
+  try {
+    const project = await handleProjectByIdFetch(req.params.id);
+    res.status(200).json({ project });
+  } catch (error: any) {
+    res.status(error.status || 500).json({ message: error.message });
+  }
+};
--- server/src/services/projectService.ts
+++ server/src/services/projectService.ts
@@ -1,8 +1,12 @@
 import Project from '../models/Project';
 import { AuthenticatedUser } from "../types/Auth";
-import { ProjectInput } from '../types/Project';
+import { IProject, ProjectInput } from '../types/Project';
 import mongoose from 'mongoose';
+import { handleThemeDeletion } from './themeService';
 
+/*
+* Handlers
+*/
 export async function handleProjectCreation(user: AuthenticatedUser, body: Partial<ProjectInput>) {
   const { title, description, primary_theme, secondary_themes, places, difficulty } = body;
 
@@ -23,6 +27,49 @@ export async function handleProjectCreation(user: AuthenticatedUser, body: Parti
   return await pushProject(projectData)
 }
 
+export async function handleAllProjectsFetch() {
+  return await Project.find().sort({ title: 1 });
+}
+
+export async function handleProjectByIdFetch(projectId: string) {
+  validateProjectId(projectId);
+  return await fetchProjectOrFail(projectId);
+}
+
+export async function handleProjectDeletion(user: AuthenticatedUser, projectId: string) {
+  validateProjectId(projectId);
+  const project = await fetchProjectOrFail(projectId);
+  enforceOwnershipOrFail(user, project);
+  return deleteProject(projectId);
+}
+
+/*
+* Operations
+*/
 async function pushProject(project : ProjectInput) {
   return Project.create(project);
+}
+
+function validateProjectId(projectId: string) {
+  if (!projectId) {
+    throw { status: 400, message: "L'identifiant du projet est requis." };
+  }
+}
+
+function enforceOwnershipOrFail(user: AuthenticatedUser, project: any) {
+  if (!project.ownerId.equals(user._id)) {
+    throw { status: 403, message: "Accès interdit. Vous n'êtes pas propriétaire de ce projet." };
+  }
+}
+
+async function deleteProject(projectId: string) {
+  return await Project.findByIdAndDelete(projectId);
+}
+
+async function fetchProjectOrFail(projectId: string) {
+  const project = await Project.findById(projectId);
+  if (!project) {
+    throw { status: 404, message: "Projet non trouvé." };
+  }
+  return project;
 }
